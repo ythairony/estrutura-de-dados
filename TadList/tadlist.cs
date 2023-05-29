b3ce8fde2d4de26cdfb6565ed800205e658f6bd7
Yuri Thairony <yurithairony@hotmail.com>
2023-05-29 17:24:27 -0300

    Incluindo TadList e TadVector

---
+++ TadList/tadlist.cs
@@ -0,0 +1,212 @@
+﻿using System;
+
+// Exception
+public class TadListException : Exception {
+  public TadListException(string message) : base(message) {}
+}
+
+public class Node {
+  private Node next;
+  private Node prev;
+  private object elem;
+
+  public Node(Node p, object e, Node n) {
+    elem = e;
+    prev = p;
+    next = n;
+  }
+
+  public void SetNext(Node n) {
+    this.next = n;
+  }
+
+  public void SetPrev(Node n) {
+    this.prev = n;
+  }
+
+  public void SetElem(object o) {
+    this.elem = o;
+  }
+
+  public Node GetNext() {
+    return next;
+  } 
+
+  public Node GetPrev() {
+    return prev;
+  }
+
+  public object GetElem() {
+    return elem;
+  }
+}
+
+
+public class TadList {
+  private int len = 0;
+  private Node head, tail;
+
+  public TadList() {
+    head = new Node(null, null, null);
+    tail = new Node(head, null, null);
+    head.SetNext(tail);
+  }
+
+  public Node findNode(object o) {
+    Node new_node = head.GetNext();
+    while (new_node.GetElem() != o) {
+      new_node = new_node.GetNext();
+      if (new_node.GetNext() == null) {
+        throw new TadListException("Object not is in this list.");
+      }
+    }
+    return new_node;
+  }
+  
+  public bool isEmpty() {
+    return head.GetNext() == tail;
+  }
+
+  public int size() {
+    return len;
+  }
+  
+  public bool isFirst(Node node) {
+    if (isEmpty()) {
+      throw new TadListException("Queue is empty");
+    }
+    return node == head.GetNext();
+  }
+
+  public Node insertFirst(object obj) {
+    Node new_first = new Node(null, obj, null);
+    new_first.SetElem(obj);
+    Node old_first;
+    old_first = head.GetNext();
+    new_first.SetNext(old_first);
+    new_first.SetPrev(head);
+    old_first.SetPrev(new_first);
+    head.SetNext(new_first);
+    this.len++;
+
+    return new_first;
+  }
+
+  public object first() {
+    return head.GetNext().GetElem();
+  }
+
+  public bool isLast(Node node) {
+    if (isEmpty()) {
+      throw new TadListException("Queue is empty");
+    }
+    return node == tail.GetPrev();
+  }
+
+  public Node insertLast(object obj) {
+    Node new_last = new Node(null, obj, null);
+    new_last.SetElem(obj);
+    Node old_last;
+
+    old_last = tail.GetPrev();
+    new_last.SetNext(tail);
+    new_last.SetPrev(old_last);
+    old_last.SetNext(new_last);
+    tail.SetPrev(new_last);
+    this.len++;
+
+    return new_last;
+  }
+
+  public object last() {
+    return tail.GetPrev().GetElem();
+  }
+
+  public void insertBefore(Node node, object obj) {
+    Node new_node = new Node(node.GetPrev(), obj, node); // arrumar
+    if (isEmpty()) {
+      throw new TadListException("Queue is empty");
+    }
+    node.GetPrev().SetNext(new_node);
+    node.SetPrev(new_node);
+    this.len++;
+  }
+
+  public void insertAfter(Node node, object obj) {
+    Node new_node = new Node(node, obj, node.GetNext()); // arrumar
+    if (isEmpty()) {
+      throw new TadListException("Queue is empty");
+    }
+    node.GetNext().SetPrev(new_node);
+    node.SetNext(new_node);
+    this.len++;
+  }
+
+  public Node before(Node node) {
+    return node.GetPrev();
+  }
+
+  public Node after(Node node) {
+    return node.GetNext();
+  }
+
+  public object replaceElement(Node old_node, object obj) {
+    object temp = old_node.GetElem();
+    old_node.SetElem(obj);
+    return temp;
+  }
+
+  public void swapElement(Node old_node, Node new_node) {
+    object obj_temp = old_node.GetElem();
+    old_node.SetElem(new_node.GetElem());
+    new_node.SetElem(obj_temp);
+  }
+
+  public object remove(Node no) {
+    object temp = no.GetElem();
+    if (isEmpty()) {
+      throw new TadListException("Queue is empty");
+    }
+    no.GetNext().SetPrev(no.GetPrev());
+    no.GetPrev().SetNext(no.GetNext());
+    this.len--;
+
+    return temp;
+  }
+
+  
+}
+
+class Program {
+  public static void Main (string[] args) {
+    TadList test = new TadList();
+    Console.WriteLine($"Está vazio? {test.isEmpty()}"); // True
+    Console.WriteLine($"Tamanho da lista = {test.size()}"); // 0
+    
+    
+    Node no1 = test.insertFirst(25);
+    Console.WriteLine($"No1 é o primeiro? {test.isFirst(no1)}"); // sim 
+    Console.WriteLine($"O primeiro elemento da lista é = {test.first()}"); // 25
+    Console.WriteLine($"Tamanho da lista = {test.size()}"); // 1
+    Node no2 = test.insertLast(40);
+    Console.WriteLine($"O último elemento da lista é = {test.last()}"); // 40
+    Console.WriteLine($"Tamanho da lista = {test.size()}"); // 2
+    Console.WriteLine($"O anterior de No1 é = {test.before(no1)}");
+    Console.WriteLine($"Removendo o No com elemento = {test.remove(no1)}");
+    Console.WriteLine($"Tamanho da lista = {test.size()}"); // 1
+    Console.WriteLine($"O primeiro elemento da lista é = {test.first()}"); // 40
+    Console.WriteLine($"O último elemento da lista é = {test.last()}"); // 40
+    test.insertAfter(no2, 93);
+    test.insertBefore(no2, "ABC");
+    Console.WriteLine($"Tamanho da lista = {test.size()}"); // 3
+    Console.WriteLine($"O primeiro elemento da lista é = {test.first()}"); // ABC
+    Console.WriteLine($"O último elemento da lista é = {test.last()}"); // 93
+    Node no3 = test.insertFirst("Coleguinha");
+    Console.WriteLine($"O primeiro elemento da lista é = {test.first()}"); // COLEGUINHA
+    Console.WriteLine($"Tamanho da lista = {test.size()}"); // 4
+    test.replaceElement(no2, 400);
+    test.swapElement(no2, no3);
+    Console.WriteLine($"O primeiro elemento da lista é = {test.first()}"); // 400
+  
+  }
+}
---
+++ TadVector/tadvector.cs
@@ -0,0 +1,174 @@
+﻿using System;
+
+public class TadVectorException : Exception {
+  public TadVectorException(string message) : base(message) {}
+}
+
+public class Node {
+  private object elem;
+  private Node next;
+  private Node prev;
+
+  public Node(Node p, object o, Node n) {
+    elem = o;
+    next = n;
+    prev = p;
+  }
+
+  public void SetNext(Node n) {
+    this.next = n;
+  }
+
+  public void SetPrev(Node n) {
+    this.prev = n;
+  }
+
+  public void SetElem(object o) {
+    this.elem = o;
+  }
+
+  public Node GetNext() {
+    return next;
+  } 
+
+  public Node GetPrev() {
+    return prev;
+  }
+
+  public object GetElem() {
+    return elem;
+  }
+}
+
+
+public class TadVector {
+  private int len = 0;
+  private Node head, tail;
+
+  public TadVector() {
+    head = new Node(null, null, null);
+    tail = new Node(head, null, null);
+    head.SetNext(tail);
+  }
+
+  public Node findNode(object o) {
+    Node new_node = head.GetNext();
+    while (new_node.GetElem() != o) {
+      new_node = new_node.GetNext();
+      if (new_node.GetNext() == null) {
+        throw new TadVectorException("Object not is in this Vector");
+      }
+    } 
+    return new_node;
+  }
+
+  public bool isEmpty() {
+    return head.GetNext() == tail;
+  }
+
+  public int size() {
+    return len;
+  }
+
+  public Node insertRank(int rank, object obj) {
+    if (obj == null) {
+      throw new TadVectorException("You cannot insert null value");
+    }
+    if (rank < 0 || rank > size()) {
+      throw new TadVectorException("Index out of range");
+    }
+    Node new_node = new Node(null, obj, null);
+    Node node = head.GetNext();
+    for (int i = 0; i < rank; i++) {
+      node = node.GetNext();
+    }
+    if (node == tail) {
+      head.SetNext(new_node);
+      tail.SetPrev(new_node);
+      new_node.SetPrev(head);
+      new_node.SetNext(tail);
+      this.len++;
+    } else {
+      new_node.SetPrev(node.GetPrev());
+      new_node.SetNext(node);
+      node.GetPrev().SetNext(new_node);
+      node.SetPrev(new_node);
+      this.len++;
+    }
+
+    return new_node;
+  }
+
+  public object elemAtRank(int rank) {
+    if (isEmpty()) {
+      throw new TadVectorException("TadVector is empty");
+    }
+    if (rank < 0 || rank >= size()) {
+      throw new TadVectorException("Index out of range");
+    }
+    Node node = head.GetNext();
+    for (int i = 0; i < rank; i++) {
+      node = node.GetNext();
+    }
+    return node.GetElem();
+  }
+
+  public object replaceAtRank(int rank, object obj) {
+    if (isEmpty()) {
+      throw new TadVectorException("TadVector is empty");
+    }
+    if (rank < 0 || rank >= size()) {
+      throw new TadVectorException("Index out of range");
+    }
+    Node node = head.GetNext();
+    for (int i = 0; i < rank; i++) {
+      node = node.GetNext();
+    }
+    object temp = node.GetElem();
+    node.SetElem(obj);
+    return temp;
+  }
+
+  public object removeAtRank(int rank) {
+    if (isEmpty()) {
+      throw new TadVectorException("TadVector is empty");
+    }
+    if (rank < 0 || rank >= size()) {
+      throw new TadVectorException("Index out of range");
+    }
+    Node node = head.GetNext();
+    for (int i = 0; i < rank; i++) {
+      node = node.GetNext();
+    }
+    node.GetPrev().SetNext(node.GetNext());
+    node.GetNext().SetPrev(node.GetPrev());
+    this.len--;
+    return node.GetElem();
+  }
+  
+}
+
+
+class Program {
+  public static void Main (string[] args) {
+    TadVector test = new TadVector();
+    Console.WriteLine($"Está vazio? {test.isEmpty()}"); // True
+    Console.WriteLine($"Tamanho da lista = {test.size()}"); // 0
+
+    Node n1 = test.insertRank(0, 25); // 1 = 25
+    Node n2 = test.insertRank(0, "ABC"); // 1 = abc, 2 = 25
+    Node n3 = test.insertRank(1, 99); // 1 abc, 2 = 99, 3 = 25
+    Node n4 = test.insertRank(0, 1); // 1 = 1 ...
+    Console.WriteLine($"Tamanho da lista = {test.size()}"); // 4
+    Console.WriteLine($"O elemento na posição 1 = {test.elemAtRank(1)}"); // ABC
+    Console.WriteLine($"O elemento na posição 0 = {test.elemAtRank(0)}"); // 1
+    Console.WriteLine($"O elemento na posição 2 = {test.elemAtRank(2)}"); // 99
+    Console.WriteLine($"O elemento na posição 3 = {test.elemAtRank(3)}"); // 25
+    Console.WriteLine($"Troque o elemento da posição 3 por 250 e retorne o antigo = {test.replaceAtRank(3, 250)}");
+    Console.WriteLine($"Remover elemento na posição 1 = {test.removeAtRank(1)}"); // 25
+    Console.WriteLine($"O elemento na posição 0 = {test.elemAtRank(0)}"); // 1
+    Console.WriteLine($"O elemento na posição 1 = {test.elemAtRank(1)}"); // 99
+    Console.WriteLine($"Tamanho da lista = {test.size()}"); // 3
+    Console.WriteLine($"Está vazio? {test.isEmpty()}"); // False
+  }
+}
